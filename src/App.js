ddb032d0359e5bace4777d7c2d61729ee177abb8
Samira26 <samira.mo85@gmail.com>
2025-06-09 13:47:03 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Samira26" target="_blank" rel="noreferrer">
-          Samira Mohamed{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/Samira26/project-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Samira26"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Samira Mohamed{" "}
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Samira26/project-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
